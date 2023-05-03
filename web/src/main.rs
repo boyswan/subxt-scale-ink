b1d488ce103024f5d6f3d1272f59cb11c95eea97
boyswan <jackwilliamboyce@gmail.com>
2023-05-03 13:32:50 +0100

    simplify

--- web/src/main.rs
+++ web/src/main.rs
@@ -1,16 +1,10 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
-use wasm_bindgen::prelude::*;
-
-use parity_scale_codec::alloc::vec;
-use parity_scale_codec::alloc::vec::Vec;
-
 use common::{Bar, Id};
 
+#[wasm_bindgen::prelude::wasm_bindgen(start)]
 fn main() {
-    wasm_bindgen_futures::spawn_local(async {
-        let to_be_used_for_subxt_contract_call = Bar {
-            inner: Id::U32(123),
-        };
-    });
+    let to_be_used_for_subxt_contract_call = Bar {
+        inner: Id::U32(123),
+    };
 }
